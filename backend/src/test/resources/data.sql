0d5923cc353abc66c77edffba0f8b0a55e841378
ADMIN <smosh.ltp@gmail.com>
2026-07-31 17:54:31 +0700

    action debug

---
+++ backend/src/test/resources/data.sql
@@ -0,0 +1,44 @@
+INSERT INTO roles(id_role, role_name)
+VALUES
+    ('R_001','ADMIN');
+
+
+INSERT INTO users(
+    id_user,
+    username,
+    password_hash,
+    role_id
+)
+VALUES
+    (
+        'U_001',
+        'ADMIN',
+        'a123456',
+        'R_001'
+    );
+
+
+INSERT INTO food_category(
+    id_category,
+    name
+)
+VALUES
+    (
+        'C_001',
+        'Chicken'
+    );
+
+
+INSERT INTO food(
+    id_food,
+    name,
+    price,
+    category_id
+)
+VALUES
+    (
+        'F_001',
+        'Chicken Burger',
+        50000,
+        'C_001'
+    );
